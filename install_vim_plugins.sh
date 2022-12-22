3618ee4402f838893e8ea33f6204102869d63850
Darren Kessner <dkessner@eitm.org>
2022-12-21 14:53:41 -1000

    changes for EI mac

--- install_vim_plugins.sh
+++ install_vim_plugins.sh
@@ -16,15 +16,17 @@ date >> vim/install.log
 
 pushd vim/pack/dk/start >> vim/install.log
 
-echo "Installing vim-latex."
-git clone https://github.com/vim-latex/vim-latex.git 2>> ../../../install.log
-echo ':helptags vim-latex/doc' | vim >> ../../../install.log 2>&1    # :help latex-suite.txt
-
 echo "Installing vim-processing."
 git clone https://github.com/sophacles/vim-processing.git 2>> ../../../install.log
 
-echo "Installing vim-vinegar."
-git clone https://github.com/tpope/vim-vinegar.git 2>> ../../../install.log
+#echo "Installing vim-latex."
+#git clone https://github.com/vim-latex/vim-latex.git 2>> ../../../install.log
+#echo ':helptags vim-latex/doc' | vim >> ../../../install.log 2>&1    # :help latex-suite.txt
+#
+#echo "Installing vim-vinegar."
+#git clone https://github.com/tpope/vim-vinegar.git 2>> ../../../install.log
+#
+#echo "Installing vim-markdown."
+#git clone https://github.com/preservim/vim-markdown.git 2>> ../../../install.log
+#
 
-echo "Installing vim-markdown."
-git clone https://github.com/preservim/vim-markdown.git 2>> ../../../install.log
